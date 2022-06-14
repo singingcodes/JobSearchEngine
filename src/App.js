94d4d9ceedcc752938a7dc2ad1177b6dd7bdac71
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-06-14 18:51:39 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import NavBar from "./components/NavBar"
-import Homepage from "./components/HomePage"
-import Company from "./components/Company"
-import JumbotronPage from "./components/JumbotronPage"
-import Category from "./components/Category"
-import Details from "./components/Details"
-import Footer from "./components/Footer"
-import { useState } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MainSearch from './components/MainSearch'
+import CompanySearchResults from './components/CompanySearchResults'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const App = () => {
-  const [search, setSearch] = useState("")
-  const setSearchData = (e) => {
-    setSearch(e.target.value)
-  }
-
+function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <JumbotronPage />
       <Routes>
-        <Route
-          path="/"
-          element={<Homepage search={search} setSearchData={setSearchData} />}
-        />
-        <Route
-          path="/company"
-          element={<Company search={search} setSearchData={setSearchData} />}
-        />
-        <Route
-          path="/category"
-          element={<Category search={search} setSearchData={setSearchData} />}
-        />
-        <Route path="/:details" element={<Details />} />
+        <Route path="/" element={<MainSearch />} />
+        <Route path="/:companyName" element={<CompanySearchResults />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   )
 }
